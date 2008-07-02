86f2334ba5f3861dd63f3255f45ecb84fa53d567
mzeijen <mzeijen@39ff8352-fd3f-0410-a04e-ba40d55a1478>
2008-07-02 13:38:48 +0000

    Added skeleton (copy from mule 1.x) for the mule 2.x part of the module

---
+++ mule2-module-smooks/mule2-module-smooks-transformer/src/main/java/org/milyn/smooks/mule/SmooksTransformer.java
@@ -0,0 +1,293 @@
+package org.milyn.smooks.mule;
+
+import static org.mule.config.i18n.MessageFactory.createStaticMessage;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.milyn.Smooks;
+import org.milyn.container.ExecutionContext;
+import org.milyn.container.plugin.PayloadProcessor;
+import org.milyn.container.plugin.ResultType;
+import org.milyn.event.report.HtmlReportGenerator;
+import org.mule.config.i18n.Message;
+import org.mule.transformers.AbstractEventAwareTransformer;
+import org.mule.umo.UMOEventContext;
+import org.mule.umo.lifecycle.InitialisationException;
+import org.mule.umo.transformer.TransformerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
+
+/**
+ * SmooksTransformer intended to be used with the Mule ESB.
+ * <p/>
+ * <h3>Usage:</h3> 
+ * <pre>
+ * Declare the tranformer in the Mule configuration file:
+ * &lt;transformers&gt;
+ *      &lt;transformer name="SmooksTransformer" className="org.milyn.smooks.mule.SmooksTransformer"/&gt;
+ * &lt;/transformers&gt;
+ *   
+ * Configure the transformer with a router:
+ * &lt;inbound-router&gt;
+ *     &lt;endpoint address="stream://System.in"  transformers="SmooksTransformer"/&gt;
+ * &lt;/inbound-router&gt;
+ *  
+ * Optional properties:
+ * &lt;property name="smooksConfig" value="smooks-config.xml" /&gt;
+ * &lt;property name="resultType" value="STRING" /&gt;
+ * &lt;property name="excludeNonSerializables" value="false" /&gt;
+ * &lt;property name="reportPath" value="/tmp/smooks-report.html" /&gt;
+ * &lt;property name="javaResultBeanId" value="orderBean" /&gt;
+ * </pre>
+ * 
+ * <h3>Description of configuration properties</h3>
+ * <ul>
+ * <li><i>smooksConfig</i> - the Smooks configuration file. Can be a path on the file system or on the classpath.
+ * <li><i>resultType</i> - type of result expected from Smooks ("STRING", "BYTES", "JAVA", "NORESULT"). Default is "STRING".
+ * <li><i>excludeNonSerializables</i> - if true, non serializable attributes from the Smooks ExecutionContext will no be included. Default is true.
+ * <li><i>reportPath</i> - specifies the path and file name for generating a Smooks Execution Report.  This is a development tool.
+ * <li><i>javaResultBeanId</i> - specifies the Smooks bean context beanId to be mapped as the result when the resultType is "JAVA".  If not specified,
+ *                               the whole bean context bean Map is mapped as the result. 
+ * </ul>
+ * 
+ * <h3>Accessing Smooks ExecutionContext attributes</h3>
+ * After Smooks has performed the filtering the transform method will make the attributes that have been set in the
+ * the ExecutionContext available for other actions in the Mule ESB.
+ * The attributes (Map) can be accessed by using the {@link #EXECUTION_CONTEXT_ATTR_MAP_KEY} key like this:
+ * <pre>
+ * umoEventContext.getMessage().get( SmooksTransformer.EXECUTION_CONTEXT_ATTR_MAP_KEY );
+ * </pre>
+ * <h3>Specifying the Source and Result Types</h3>
+ * From the object payload data type, this Transformer is able to automatically determine the type of
+ * {@link javax.xml.transform.Source} to use (via the Smooks {@link PayloadProcessor}).  The
+ * {@link javax.xml.transform.Result} type to be used can be specified via the "resultType"
+ * property, as outlined above.
+ * <p/>
+ * It is expected that the above mechanism will be satisfactory for most usecase, but not all.
+ * For the other usecases, this action supports {@link org.milyn.container.plugin.SourceResult}
+ * payloads. This allows you to manually specify other Source and Result
+ * types, which is of particular interest with respect to the Result type e.g. for streaming
+ * the Result to a file etc.
+ * 
+ * @author <a href="mailto:daniel.bevenius@gmail.com">Daniel Bevenius</a>				
+ *
+ */
+public class SmooksTransformer extends AbstractEventAwareTransformer
+{
+	public static final String EXECUTION_CONTEXT_ATTR_MAP_KEY = "SmooksExecutionContext";
+	
+	private static final long serialVersionUID = 1L;
+	
+	private final Logger log = LoggerFactory.getLogger( SmooksTransformer.class );
+	
+	/*
+	 * Smooks payload processor 
+	 */
+	private PayloadProcessor payloadProcessor;
+	
+	/*
+	 * Smooks instance
+	 */
+	private Smooks smooks;
+	
+	/*
+	 * The expected result type.
+	 */
+	private String resultType;
+	
+	/*
+	 * Filename for smooks configuration. Default is smooks-config.xml
+	 */
+    private String smooksConfigFile;
+
+    /*
+     * If true, non serializable attributes from the Smooks ExecutionContext will no be included. Default is true.
+     */
+	private boolean excludeNonSerializables = true;
+
+	/*
+	 * Path where the Smooks Report will be generated.
+	 */
+	private String reportPath;
+
+	/*
+	 * Bean Id under which the JavaResult bean will be bound
+	 */
+	private String javaResultBeanId;
+
+    //	public
+	
+	public void initialise() throws InitialisationException
+	{
+		//	determine the ResultType
+		ResultType resultType = getResultType();
+		
+		//	Create the Smooks instance
+		smooks = createSmooksInstance();
+		
+		//	Create the Smooks payload processor
+		payloadProcessor = new PayloadProcessor( smooks, resultType );
+		
+		//	set the JavaResult beanId if specified
+		if ( resultType == ResultType.JAVA ) 
+		{
+            if ( javaResultBeanId != null ) 
+            {
+                payloadProcessor.setJavaResultBeanId( javaResultBeanId );
+            }
+        }
+	}
+
+	public String getSmooksConfig()
+	{
+		return smooksConfigFile;
+	}
+	
+	public void setSmooksConfig( final String smooksConfigFile )
+	{
+		this.smooksConfigFile = smooksConfigFile;
+	}
+	
+    public Object clone() throws CloneNotSupportedException
+    {
+    	return this;
+    }
+
+	public void setResultType( final String resultType )
+	{
+		this.resultType = resultType;
+	}
+	
+    /**
+     * @param excludeNonSerializables  - If true, non serializable attributes from the Smooks ExecutionContext will no be included. Default is true.
+     */
+	public void setExcludeNonSerializables( boolean excludeNonSerializables )
+	{
+		this.excludeNonSerializables = excludeNonSerializables;
+	}
+	
+	public void setReportPath( final String reportPath )
+	{
+		this.reportPath = reportPath;
+	}
+
+	public void setJavaResultBeanId( final String javaResultBeanId )
+	{
+		this.javaResultBeanId = javaResultBeanId;
+	}
+	
+	//	protected
+	
+	@Override
+	public Object transform( final Object payload, String encoding, UMOEventContext umoEventContext ) throws TransformerException
+	{
+        //	Create Smooks ExecutionContext.
+		ExecutionContext executionContext = smooks.createExecutionContext();
+		
+		//	Add smooks reporting if configured
+		addReportingSupport( executionContext );
+		
+        //	Use the Smooks PayloadProcessor to execute the transformation....	
+        final Object transformedPayload = payloadProcessor.process( payload, executionContext );
+        
+        //	Set the Smooks Excecution properties on the Mule Message object
+        umoEventContext.getMessage().setProperty( EXECUTION_CONTEXT_ATTR_MAP_KEY, getSerializableObjectsMap( executionContext.getAttributes() ) );
+        
+		return transformedPayload;
+	}
+	
+	/**
+     * Will return a Map containing only the Serializable objects 
+     * that exist in the passed-in Map if {@link #excludeNonSerializables} is true.
+     * 
+     * @param smooksAttribuesMap 	- Map containing attributes from the Smooks ExecutionContext
+     * @return Map	- Map containing only the Serializable objects from the passed-in map.
+     */
+    @SuppressWarnings( "unchecked" )
+	protected Map getSerializableObjectsMap( final Map smooksAttribuesMap )
+	{
+    	if ( !excludeNonSerializables )
+    		return smooksAttribuesMap;
+    	
+		Map smooksExecutionContextMap = new HashMap();
+		
+		Set<Map.Entry> s = smooksAttribuesMap.entrySet();
+		for (Map.Entry me : s) 
+		{
+			Object value = me.getValue();
+			if( value instanceof Serializable )
+			{
+				smooksExecutionContextMap.put( me.getKey(), value );
+			}
+		}
+		return smooksExecutionContextMap;
+	}
+	
+	//	private
+	
+	private Smooks createSmooksInstance() throws InitialisationException
+	{
+		if ( smooksConfigFile == null )
+		{
+			final Message errorMsg = createStaticMessage( "'smooksConfigFile' parameter must be specified" );
+			throw new InitialisationException( errorMsg, this );
+		}
+	
+		try
+		{
+			return new Smooks ( smooksConfigFile );
+		} 
+		catch ( final IOException e)
+		{
+			final Message errorMsg = createStaticMessage( "IOException while trying to get smooks instance: " );
+			throw new InitialisationException( errorMsg, e);
+		} 
+		catch ( final SAXException e)
+		{
+			final Message errorMsg = createStaticMessage( "SAXException while trying to get smooks instance: " );
+			throw new InitialisationException( errorMsg, e );
+		}
+	}
+	
+	private ResultType getResultType() throws InitialisationException
+	{
+		ResultType resultType = ResultType.STRING;
+		if ( this.resultType != null )
+		{
+	        try 
+	        {
+	            resultType = ResultType.valueOf( this.resultType );
+	        } 
+	        catch ( final IllegalArgumentException e ) 
+	        {
+    			final Message errorMsg = createStaticMessage( "Invalid 'resultType' config value '" + resultType + "'.  Valid values are: " + Arrays.asList(ResultType.values() ) );
+	            throw new InitialisationException(errorMsg, e );
+	        }
+		}
+		return resultType;
+	}
+	
+	private void addReportingSupport( final ExecutionContext executionContext ) throws TransformerException 
+	{
+		if( reportPath != null ) 
+		{
+            try 
+            {
+            	log.info( "Using Smooks Reporting. Will generate report in file [" + reportPath  + "]. Do not use in production evironment as this will have negative impact on performance!");
+                executionContext.setEventListener( new HtmlReportGenerator( reportPath ) );
+            } 
+            catch ( final IOException e) 
+            {
+    			final Message errorMsg = createStaticMessage( "Failed to create HtmlReportGenerator instance." );
+	            throw new TransformerException( errorMsg, e );
+            }
+        }
+	}
+
+}
---
+++ mule2-module-smooks/mule2-module-smooks-transformer/src/test/java/org/milyn/smooks/mule/SmooksTransformerTest.java
@@ -0,0 +1,135 @@
+package org.milyn.smooks.mule;
+
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.milyn.io.StreamUtils;
+import org.mule.impl.RequestContext;
+import org.mule.tck.MuleTestUtils;
+import org.mule.umo.UMOEvent;
+import org.mule.umo.UMOEventContext;
+import org.mule.umo.lifecycle.InitialisationException;
+import org.mule.umo.transformer.TransformerException;
+
+/**
+ * Unit test for {@link SmooksTransformer}
+ * <p/>
+ * The test in the class intentionally only test the configuration and <br>
+ * execution of {@link SmooksTransformer} and not the actual tranformations<br>
+ * that Smooks performs as these are covered in the Smooks project.
+ * 
+ * @author <a href="mailto:daniel.bevenius@gmail.com">Daniel Bevenius</a>				
+ *
+ */
+public class SmooksTransformerTest 
+{
+	private SmooksTransformer smooksTransformer;
+
+	private UMOEventContext eventContext;
+	
+	private final String smooksConfigFile = "smooks-config.xml";
+	
+	@Test ( expected = InitialisationException.class )
+	public void initWithoutSmooksConfigFile() throws TransformerException, InitialisationException
+	{
+		smooksTransformer.setSmooksConfig( null );
+		smooksTransformer.initialise();
+	}
+	
+	@Test ( expected = InitialisationException.class )
+	public void illegalResultType() throws TransformerException, InitialisationException
+	{
+		smooksTransformer.setSmooksConfig( smooksConfigFile );
+		smooksTransformer.setResultType( "badResultType" );
+		smooksTransformer.initialise();
+	}
+	
+	@Test 
+	public void javaResultBeanId() throws TransformerException
+	{
+		smooksTransformer.setSmooksConfig( smooksConfigFile );
+		smooksTransformer.setResultType( "JAVA" );
+		smooksTransformer.setJavaResultBeanId( "beanId" );
+		try
+		{
+			smooksTransformer.initialise();
+		} 
+		catch (InitialisationException e)
+		{
+			fail( "Should not have thrown A InitializationException");
+		}
+	}
+	
+	@Test
+	public void doTransformation() throws TransformerException
+	{
+		smooksTransformer.setSmooksConfig( smooksConfigFile );
+		smooksTransformer.setExcludeNonSerializables( false );
+		byte[] inputMessage = readInputMessage();
+		Object transformedObject = smooksTransformer.transform( inputMessage, "UTF-8", eventContext );
+		assertNotNull ( transformedObject );
+		Object attributes = eventContext.getMessage().getProperty( SmooksTransformer.EXECUTION_CONTEXT_ATTR_MAP_KEY );
+		assertNotNull( attributes );
+	}
+	
+	@Test
+	public void doTransformationWithSmooksReportGeneration() throws TransformerException, InitialisationException
+	{
+		File reportFile = new File ( "target" + File.separator + "smooks-report.html" );
+		smooksTransformer.setSmooksConfig( smooksConfigFile );
+		smooksTransformer.setReportPath( reportFile.getAbsolutePath() );
+		smooksTransformer.initialise();
+		byte[] inputMessage = readInputMessage();
+		try
+		{
+    		Object transformedObject = smooksTransformer.transform( inputMessage, "UTF-8", eventContext );
+    		assertNotNull ( transformedObject );
+			assertTrue( reportFile.exists() );
+		}
+		finally
+		{
+			if ( reportFile.exists() )
+			{
+				reportFile.delete();
+			}
+		}
+	}
+	
+	@Before
+	public void setUp() throws Exception
+	{
+    	smooksTransformer = new SmooksTransformer();
+		smooksTransformer.setSmooksConfig( smooksConfigFile );
+		smooksTransformer.initialise();
+		RequestContext.setEvent( getTestEvent ( "Test!" ) );	
+		eventContext = RequestContext.getEventContext();
+	}
+	
+	//	private 
+	
+	private static byte[] readInputMessage() 
+	{
+        try 
+        {
+            return StreamUtils.readStream( SmooksTransformerTest.class.getResourceAsStream( "/input-message.xml"));
+        } 
+        catch (IOException e) 
+        {
+        	e.printStackTrace();
+            return "<no-message/>".getBytes();
+        }
+    }
+	
+	private static UMOEvent getTestEvent(Object data) throws Exception
+    {
+        return MuleTestUtils.getTestEvent(data);
+    }
+	
+}
