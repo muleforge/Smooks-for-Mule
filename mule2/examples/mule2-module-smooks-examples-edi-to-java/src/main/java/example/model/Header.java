e1ad5a9051391a9a993a0e01e3a3c2f673597db0
mzeijen <mzeijen@39ff8352-fd3f-0410-a04e-ba40d55a1478>
2008-07-11 21:05:44 +0000

    - configured maven-license-plugin to set the license header for me - set the license everywhere with the license plugin

--- mule2/examples/mule2-module-smooks-examples-edi-to-java/src/main/java/example/model/Header.java
+++ mule2/examples/mule2-module-smooks-examples-edi-to-java/src/main/java/example/model/Header.java
@@ -1,18 +1,19 @@
-/*
-	Milyn - Copyright (C) 2006
+/**
+ * Copyright (C) 2008 Maurice Zeijen <maurice@zeijen.net>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-	This library is free software; you can redistribute it and/or
-	modify it under the terms of the GNU Lesser General Public
-	License (version 2.1) as published by the Free Software
-	Foundation.
-
-	This library is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-	See the GNU Lesser General Public License for more details:
-	http://www.gnu.org/licenses/lgpl.txt
-*/
 package example.model;
 
 import java.math.BigDecimal;
